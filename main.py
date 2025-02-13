bb01021462a74b85bf38d87d8dccde10fb4e95d1
Danil Gasimov <dengad2003@yandex.ru>
2025-02-13 23:16:35 +0300

    commit

--- main.py
+++ main.py
@@ -1,8 +1,29 @@
 import pandas as pd
 
-steam_games = pd.read_csv('steam_games.csv')
-steam_prices = pd.read_csv('steam_prices.csv')
 
-merged_df = pd.merge(steam_games, steam_prices, on='Title', how='inner')  # inner join, чтобы оставить только совпадающие игры
+# Объединение баз данных ()
+# steam_games = pd.read_csv('steam_games.csv')
+# steam_prices = pd.read_csv('steam_prices.csv')
+# merged_df = pd.merge(steam_games, steam_prices, on='Title', how='inner')  # inner join, чтобы оставить только совпадающие игры
+# print(merged_df[['Title','genres','Original Price']])
+# merged_df.to_csv('my_steam_data.csv', index=False)
 
-print(merged_df[['Title','genres','Original Price']])
+my_steam_data = pd.read_csv('data_frames/my_steam_data.csv')
+
+for index, row in my_steam_data.iterrows():
+    title_genres_list = [index, row['Title'], row['Genres']]
+    # print(title_genres_list[2])
+    if 'Indie'in title_genres_list[2]:
+        print([title_genres_list[1], True])
+    else:
+        print([title_genres_list[1], False])
+    
+
+
+
+# n = 0
+# for genres in my_steam_data['Genres']:
+#     if 'Indie' in genres:
+#         print(True)
+        
+# print(n)
