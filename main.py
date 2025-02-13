0f157bcbdaeccfb43bec34dd2551ec04da2c2c9d
Danil Gasimov <dengad2003@yandex.ru>
2025-02-13 23:19:01 +0300

    commit

--- main.py
+++ main.py
@@ -10,20 +10,22 @@ import pandas as pd
 
 my_steam_data = pd.read_csv('data_frames/my_steam_data.csv')
 
-for index, row in my_steam_data.iterrows():
-    title_genres_list = [index, row['Title'], row['Genres']]
-    # print(title_genres_list[2])
-    if 'Indie'in title_genres_list[2]:
-        print([title_genres_list[1], True])
-    else:
-        print([title_genres_list[1], False])
-    
-
-
-
-# n = 0
-# for genres in my_steam_data['Genres']:
-#     if 'Indie' in genres:
-#         print(True)
+def main():
+    for index, row in my_steam_data.iterrows():
+        title_genres_list = [index, row['Title'], row['Genres']]
+        # print(title_genres_list[2])
+        if 'Indie'in title_genres_list[2]:
+            print([title_genres_list[1], True])
+        else:
+            print([title_genres_list[1], False])
         
-# print(n)
+
+    # n = 0
+    # for genres in my_steam_data['Genres']:
+    #     if 'Indie' in genres:
+    #         print(True)
+            
+    # print(n)
+
+if __name__ == "__main__":
+    main()
