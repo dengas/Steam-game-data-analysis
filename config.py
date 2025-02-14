7180811bb354ed7143ef9f64d2fbddc9efe553d5
Danil Gasimov <dengad2003@yandex.ru>
2025-02-14 20:58:40 +0300

    commit

--- config.py
+++ config.py
@@ -2,4 +2,4 @@
 URL_1 = "https://www.kaggle.com/datasets/srgiomanhes/steam-games-dataset-2025"
 
 # Все жанры
-ALL_GENRES = ['Racing', 'Indie', 'Massively Multiplayer', 'Photo Editing', 'Action', 'Movie', 'Casual', 'Free To Play', 'Web Publishing', 'Design & Illustration', 'Utilities', 'Documentary', 'Audio Production', 'Software Training', 'Early Access', 'Simulation', 'Short', 'Tutorial', 'Gore', '360 Video', 'Game Development', 'Sexual Content', 'Episodic', 'Adventure', 'Education', 'Animation & Modeling', 'Accounting', 'Sports', 'Violent', 'Nudity', 'Strategy', 'Video Production', 'RPG']
+ALL_GENRES = {'Racing', 'Indie', 'Massively Multiplayer', 'Photo Editing', 'Action', 'Movie', 'Casual', 'Free To Play', 'Web Publishing', 'Design & Illustration', 'Utilities', 'Documentary', 'Audio Production', 'Software Training', 'Early Access', 'Simulation', 'Short', 'Tutorial', 'Gore', '360 Video', 'Game Development', 'Sexual Content', 'Episodic', 'Adventure', 'Education', 'Animation & Modeling', 'Accounting', 'Sports', 'Violent', 'Nudity', 'Strategy', 'Video Production', 'RPG'}
--- main.py
+++ main.py
@@ -3,57 +3,23 @@ import config
 import ast
 
 
-steam_games_df = pd.read_csv('data_frames/steam_games_df.csv')
-
-# def main():
-#     n = 0
-#     price_all = 0
-#     for index, row in steam_games_df.iterrows():
-#         if row['is_released'] is True and 'Indie'in row['genres']:
-#             print([row['name'], row['price(USD)'], True])
-#             n+=1
-#             price = row['price(USD)']
-#             price_all = price_all + price
-#         else:
-#             print([row['name'], row['price(USD)'], False])
-
-#     print(f"Количество игр в жанре Indie: {n}")
-#     print(f"{round(price_all, 2)}$")
-
-# if __name__ == "__main__":
-#     main()
 
 steam_games_df = pd.read_csv('data_frames/steam_games_df.csv')
-all_genres = config.ALL_GENRES
 
 def main():
+    all_genres = config.ALL_GENRES
     
-    genre_stats = {genre: {'count': 0, 'revenue': 0.0} for genre in all_genres}
-    n = 0
-    price_all = 0
+    genre_counts = {genre: {'count': 0, 'price': 0} for genre in all_genres}
     for index, row in steam_games_df.iterrows():
         if row['is_released'] is True:
-            price = row['price(USD)']
-            game_genres = ast.literal_eval(row['genres'])
-
-        for genre in all_genres:
-            if genre in game_genres:
-                genre_stats[genre]['count'] += 1
-                genre_stats[genre]['revenue'] += price
-                
-                
-    for genre, stats in genre_stats.items():
-        if stats['count'] > 0:
-            print(f"Жанр: {genre}")
-            print(f"Количество игр: {stats['count']}")
-            print(f"Общий доход: {round(stats['revenue'], 2)}$")
-            print("-" * 30)
-    
-    # Получаем строки, которые являются дубликатами по столбцу 'name'
-    duplicates_df = steam_games_df[steam_games_df.duplicated(subset='name', keep=False)]
-
-    # Выводим результат
-    print(duplicates_df)
+            for genre in all_genres:
+                if genre in ast.literal_eval(row['genres']):
+                    genre_counts[genre]['count'] += 1
+                    genre_counts[genre]['price'] += row['price(USD)']
+    for genre, data in genre_counts.items():
+        print(f"Количество игр в жанре {genre}: {data['count']}")
+        print(f"Общая стоимость игр в жанре {genre}: {round(data['price'], 2)}$")
+        print("-" * 30)
 
 if __name__ == "__main__":
     main()
